15c007aadf978a735f0cdcd91e1fc779602d1c4e
Zetso <himamax31@gmail.com>
2026-09-19 22:40:48 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "yasmina-booking-v5";
-const SHELL = ["./", "index.html", "logo-full-dark.png", "manifest.webmanifest", "icons/icon-192.png", "icons/icon-512.png"];
+const CACHE = "yasmina-booking-v6";
+const SHELL = ["./", "index.html", "logo-full-dark.png", "manifest.webmanifest", "icons/icon-192.png", "icons/icon-512.png", "icons/icon-maskable-512.png"];
 self.addEventListener("install", e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting())); });
 self.addEventListener("activate", e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
 self.addEventListener("fetch", e => { const url=new URL(e.request.url); if(e.request.method!=="GET" || url.origin!==location.origin)return; e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))); });
